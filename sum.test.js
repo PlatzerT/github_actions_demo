2d6308208b3ad2364055046121f03dc06693ddf6
PlatzerT <thpl.school@gmail.com>
2023-02-17 08:20:30 +0100

    changed numbers

--- sum.test.js
+++ sum.test.js
@@ -1,7 +1,7 @@
 const sum = require("./sum");
 
-test("adds 2 + 4 to equal 6", () => {
-  expect(sum(2, 4)).toBe(6);
+test("adds 1 + 5 to equal 6", () => {
+  expect(sum(1, 5)).toBe(6);
 });
 
 test("adds 6 + 6 to equal not 10", () => {
